14446b55bba7e39cb0c961a08d5f3453505363a3
Francisco Dias <fdias@hubspot.com>
2016-03-22 19:11:15 -0400

    commented out for loop & added `;`

--- sketch.js
+++ sketch.js
@@ -11,18 +11,14 @@ function setup() {
   for (var i = 0; i < dataLength; i++) {
     dataArr.push(new Adata(data[i]));
   }
-
   createCanvas(500,500);
 }
 
-
-
 function draw() {
-  background('#333')
-  for (item in dataArr){
-  }
+  background('#333');
+  // for (item in dataArr){
+  // }
 
   fill("#fefefe")
   text("Intro text", 250, 250);
-
 }
