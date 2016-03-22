0de1c2b76233595e386e4ae3e499924b5feb01a2
Francisco Dias <fdias@hubspot.com>
2016-03-22 19:06:51 -0400

    added scaffolding

--- sketch.js
+++ sketch.js
@@ -1,7 +1,23 @@
-function setup() {
+var Adata = function(height) {
+  this.height = height;
 
 }
 
+var dataLength = data.length;
+var dataArr = [];
+
+
+function setup() {
+  for (var i = 0; i < dataLength; i++) {
+    dataArr.push(new Adata(data[i]));
+  }
+}
+
+
+
 function draw() {
-  
+  for (item in dataArr){
+    
+  }
+
 }
