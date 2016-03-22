56605a9927e2fbfe19c540c3cc36ec53682950f6
Francisco Dias <fdias@hubspot.com>
2016-03-22 19:09:08 -0400

    added text

--- sketch.js
+++ sketch.js
@@ -1,23 +1,28 @@
+
+
 var Adata = function(height) {
   this.height = height;
-
 }
 
 var dataLength = data.length;
 var dataArr = [];
 
-
 function setup() {
   for (var i = 0; i < dataLength; i++) {
     dataArr.push(new Adata(data[i]));
   }
+
+  createCanvas(500,500);
 }
 
 
 
 function draw() {
+  background('#333')
   for (item in dataArr){
-    
   }
+
+  fill("#fefefe")
+  text("Intro text", 250, 250);
 
 }
