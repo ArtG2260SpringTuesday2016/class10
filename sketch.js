576eed05f1d1a7d6eae2d46fcdbd85495a934655
Francisco Dias <fdias@hubspot.com>
2016-03-22 20:03:27 -0400

    added match stick heads & font

--- sketch.js
+++ sketch.js
@@ -1,31 +1,49 @@
 
+var dataLength = data.length;
+var dataArr = [];
+var dataArrLength = dataArr.length;
+var dataValueText = "Hover over match stick head to see values";
+
 
 var Adata = function(localHeight) {
   this.height = localHeight;
   this.width = 10;
+  this.dramaticHeight = this.height * -5
+
   this.renderRect = function(x) {
-    // x,y,w,h
-    fill("#bada55")
-    rect(x,height,this.width,this.height * -5)
+    fill("#bada55");
+    rect(x * 10,height,this.width,this.dramaticHeight);
+  };
+
+  this.renderCircleTop = function(x) {
+    var circleWidth = this.width * 2
+    fill("hotpink");
+    ellipse((x * 10) + (this.width / 2), height + this.dramaticHeight, circleWidth, circleWidth)
+  };
+
+  this.render = function(i) {
+    this.renderRect(i)
+    this.renderCircleTop(i)
   }
 }
 
-var dataLength = data.length;
-var dataArr = [];
 
 function setup() {
   for (var i = 0; i < dataLength; i++) {
     dataArr.push(new Adata(data[i]));
   }
   createCanvas(500,500);
+  dataArrLength = dataArr.length;
 }
 
 function draw() {
   background('#333');
-  for (var i = 0; i < dataArr.length; i++){
-    dataArr[i].renderRect(i * 10)
+  for (var i = 0; i < dataArrLength; i++){
+    dataArr[i].render(i);
   }
 
   fill("#fefefe")
-  text("Intro text", 20, 100);
+  textSize(22)
+  // textFont("Bangers")
+  text(dataValueText, 20, 100);
 }
