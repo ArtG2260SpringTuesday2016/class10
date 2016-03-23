d10280415be85ea3ec4ca7ab59f936a8bb9c95ab
Francisco Dias <fdias@hubspot.com>
2016-03-22 20:24:18 -0400

    added interactivity

--- sketch.js
+++ sketch.js
@@ -9,22 +9,32 @@ var Adata = function(localHeight) {
   this.height = localHeight;
   this.width = 10;
   this.dramaticHeight = this.height * -5
+  this.x;
+
+  var circleWidth = this.width * 2
 
   this.renderRect = function(x) {
     fill("#bada55");
-    rect(x * 10,height,this.width,this.dramaticHeight);
+    rect(x,height,this.width,this.dramaticHeight);
   };
 
   this.renderCircleTop = function(x) {
-    var circleWidth = this.width * 2
     fill("hotpink");
-    ellipse((x * 10) + (this.width / 2), height + this.dramaticHeight, circleWidth, circleWidth)
+    ellipse((x) + (this.width / 2), height + this.dramaticHeight, circleWidth, circleWidth)
   };
 
-  this.render = function(i) {
-    this.renderRect(i)
-    this.renderCircleTop(i)
+  this.isMouseClose = function(posnX, posnY) {
+    if (dist(posnX, posnY, this.x, height + this.dramaticHeight) < circleWidth)
+      dataValueText = this.height
   }
+
+  this.render = function(iterator) {
+    this.x = iterator * 10;
+    this.renderRect(this.x)
+    this.renderCircleTop(this.x)
+    this.isMouseClose(mouseX, mouseY)
+  }
+
 }
 
 
