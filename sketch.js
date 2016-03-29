3038b05e785b13e2ad80931515ab287b70368e10
FlyingPig13 <anotherhale.m@gmail.com>
2016-03-29 16:31:45 -0400

    Added and linked library
    Using the graph example from class as a base I added and linked
    collide2d.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,56 @@
-function setup() {
+var dataLength = data.length;
+var dataArr = [];
+var dataArrLength = dataArr.length;
+var dataValueText = "Hover over match stick head to see values";
 
+
+var Adata = function(localHeight) {
+  this.height = localHeight;
+  this.width = 10;
+  this.dramaticHeight = this.height * -5
+
+  var circleWidth = this.width * 2
+
+  this.renderRect = function(x) {
+    fill("#bada55");
+    rect(x,height,this.width,this.dramaticHeight);
+  };
+
+  this.renderCircleTop = function(x) {
+    fill("hotpink");
+    ellipse((x) + (this.width / 2), height + this.dramaticHeight, circleWidth, circleWidth)
+  };
+
+  this.isMouseCloseToCircle = function(posnX, posnY, x) {
+    if (dist(posnX, posnY, x, height + this.dramaticHeight) < circleWidth) {
+      dataValueText = this.height
+    }
+  }
+
+  this.render = function(iterator) {
+    var x = iterator * 10;
+    this.renderRect(x)
+    this.renderCircleTop(x)
+    this.isMouseCloseToCircle(mouseX, mouseY, x)
+  }
+}
+
+function setup() {
+  for (var i = 0; i < dataLength; i++) {
+    dataArr.push(new Adata(data[i]));
+  }
+  createCanvas(500,500);
+  dataArrLength = dataArr.length;
 }
 
 function draw() {
-  
+  background('#333');
+  for (var i = 0; i < dataArrLength; i++){
+    dataArr[i].render(i);
+  }
+
+  fill("#fefefe")
+  textSize(22)
+  // textFont("Bangers")
+  text(dataValueText, 20, 100);
 }
